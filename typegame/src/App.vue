67cfa069f22ea701a5bff2447a45b3a5d45f44c1
stimulateDxm <1659513605@qq.com>
2022-03-13 15:39:56 +0800

    typinggame

---
+++ typegame/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+     <Eliminate />
+  </div>
+</template>
+
+<script>
+
+import Eliminate from "./components/eliminate"
+
+export default {
+  name: 'App',
+  components:{Eliminate},
+}
+</script>
+
+
